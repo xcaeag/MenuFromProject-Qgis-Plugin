49ee1e333ead6c943cc3d7c67c755e073c1bc311
Tokimasasogo <112603395+Tokimasasogo@users.noreply.github.com>
2022-09-01 15:48:27 +0900

    Create ja.ts

---
+++ menu_from_project/i18n/ja.ts
@@ -0,0 +1,188 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>ConfDialog</name>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="29"/>
+        <source>Projects</source>
+        <translation>プロジェクト</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="127"/>
+        <source>Edit</source>
+        <translation>編集</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="132"/>
+        <source>Name</source>
+        <translation>名前</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="137"/>
+        <source>Type</source>
+        <translation>形式</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="142"/>
+        <source>Location</source>
+        <translation>座標</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="147"/>
+        <source>QGIS Project</source>
+        <translation>QGISプロジェクト</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="177"/>
+        <source>Add new project</source>
+        <translation>新規レイヤを追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="180"/>
+        <source>Add</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="199"/>
+        <source>Delete selected</source>
+        <translation>選択を取り消し</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="202"/>
+        <source>Del</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="221"/>
+        <source>Move up selected project</source>
+        <translation>選択したプロジェクトを上位に移動</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="224"/>
+        <source>Up</source>
+        <translation>上に移動</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="243"/>
+        <source>Move down selected project</source>
+        <translation>選択したプロジェクトを下位に移動</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="246"/>
+        <source>Down</source>
+        <translation>下に移動</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="286"/>
+        <source>Options</source>
+        <translation>オプション</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="293"/>
+        <source>Load all layers item</source>
+        <translation>すべてのレイヤ項目を読み込む</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="319"/>
+        <source>Create group</source>
+        <translation>グループを作成</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="349"/>
+        <source>Show title &amp;&amp; abstract in tooltip. Priority to :</source>
+        <translation>タイトルと抄録をツールチップに表示する。優先順位：</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="368"/>
+        <source>OGC QGis Server metadata</source>
+        <translation>OGC QGisサーバーメタデータ</translation>
+    </message>
+    <message>
+        <location filename="../ui/conf_dialog.ui" line="384"/>
+        <source>Layer metadata</source>
+        <translation>レイヤメタデータ</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="72"/>
+        <source>New menu</source>
+        <translation>新規メニュー</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="76"/>
+        <source>Add layer menu</source>
+        <translation>レイヤメニューを追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="82"/>
+        <source>Merge with previous</source>
+        <translation>従来のものと統合</translation>
+    </message>
+</context>
+<context>
+    <name>MenuConfDialog</name>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="110"/>
+        <source>Add from file</source>
+        <translation>ファイルから追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="115"/>
+        <source>Add from PostgreSQL database</source>
+        <translation>PostgreSQLデータベースから追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="120"/>
+        <source>Add from URL</source>
+        <translation>URLから追加</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="141"/>
+        <location filename="../ui/menu_conf_dlg.py" line="354"/>
+        <source>Use project title</source>
+        <translation>プロジェクトのタイトルを使用</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="272"/>
+        <source>No HTTP Browser, simply paste your URL into the &apos;project&apos; column.</source>
+        <translation>HTTPブラウザを使わず、「プロジェクト」欄にURLを貼り付ける。</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="573"/>
+        <source>Edit this project</source>
+        <translation>このプロジェクトを編集</translation>
+    </message>
+</context>
+<context>
+    <name>MenuFromProject</name>
+    <message>
+        <location filename="../menu_from_project.py" line="501"/>
+        <location filename="../menu_from_project.py" line="845"/>
+        <source>Load all</source>
+        <translation>すべてを読み込む</translation>
+    </message>
+    <message>
+        <location filename="../menu_from_project.py" line="670"/>
+        <source>Projects configuration</source>
+        <translation>プロジェクトのコンフィグ</translation>
+    </message>
+    <message>
+        <location filename="../menu_from_project.py" line="685"/>
+        <source>Help</source>
+        <translation>ヘルプ</translation>
+    </message>
+</context>
+<context>
+    <name>menu_from_project</name>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="241"/>
+        <source>Projects configuration</source>
+        <translation>プロジェクトのコンフィグ</translation>
+    </message>
+    <message>
+        <location filename="../ui/menu_conf_dlg.py" line="245"/>
+        <source>QGIS projects (*.qgs *.qgz)</source>
+        <translation>QGISプロジェクト(*.qgs *.qgz)</translation>
+    </message>
+</context>
+</TS>
